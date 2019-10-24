9fff16a5c6274a9455bf9c90382a0e290672fb5d
bekeplar <bekeplar@gmail.com>
2019-10-24 04:32:55 -0700

    Completed module

---
+++ src/app/product.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { getLocaleNumberSymbol } from '@angular/common';
+
+@Injectable()
+export class ProductService {
+
+  constructor(private _http: Http) {
+    getAlbum(id: number) {
+
+    }
+  }
+
+}
